1494301cf5626e9a8f3a79f56eb0c9d6f6617d8f
Ken Rector <kenneth.rector.2go@gmail.com>
2026-03-06 20:25:50 -0600

    Point dev email API to local send-email-sms endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://invoice.securityassignments.com/',
-  emailApiUrl: 'https://messagesender.assignpros.com/send-email',
+  emailApiUrl: 'http://localhost:3000/send-email-sms',
+  //emailApiUrl: 'https://messagesender.assignpros.com/send-email-sms',
   captchaSiteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
 };
