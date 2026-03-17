57905abf2e3a2d873361a61536a58b5625a5374a
Ken Rector <kenneth.rector.2go@gmail.com>
2026-03-16 22:45:29 -0500

    Fixing more AI crap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://invoice.securityassignments.com/',
+  apiUrl: 'https://API_InternalAdmin.SecurityAssignments.com/',
   emailApiUrl: 'https://messagesender.assignpros.com/send-email-sms',
   captchaSiteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://invoice.securityassignments.com/',
+  apiUrl: 'https://api.InternalAdmin.securityassignments.com/',
   emailApiUrl: 'http://localhost:3000/send-email-sms',
   //emailApiUrl: 'https://messagesender.assignpros.com/send-email-sms',
   captchaSiteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
